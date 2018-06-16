75bb6404be691e22badab80af319a86f720c5d25
Maneesha <venigallamaneesha24@gmail.com>
2018-06-16 06:10:16 +0530

    words updated

--- wordsModule.js
+++ wordsModule.js
@@ -1,12 +1,17 @@
-var wordsModule = (function(){
-	var words = [
-		
+var wordsModule = (function () {
+	var words = ["Nine noble families fight for control of the mythical land of Westeros. Political and sexual intrigue is pervasive. Robert Baratheon, King of Westeros, asks his old friend Eddard, Lord Stark, to serve as Hand of the King, or highest official. Secretly warned that the previous Hand was assassinated, Eddard accepts in order of business to investigate further. Meanwhile the Queen's family, the Lannisters, may be hatching a plot to take power. Across the sea, the last members of the previous and deposed ruling family, the Targaryens, are also scheming to regain the throne. The friction between the houses Stark, Lannister, Baratheon and Targaryen and with the remaining great houses Greyjoy, Tully, Arryn, Tyrell and Martell leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war and political confusion, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+
+
+		"Gotham's new district attorney has been elected. His name is Harvey Dent, and he has a radical new agenda that threatens to take down Gotham's organized crime underworld once and for all with an iron fist. But the emergence of the rogue vigilante known as Batman has caused problems for Dent and his agenda. A new criminal mastermind known only as &#39;The Joker&#39; has arrived and aims to take Gotham out from under Harvey Dent's iron fist. The Joker stages a masterfully planned bank robbery and robs the Gotham mob blind. He uses this money to stage a series of horrific and strategic attacks against the city and its people - each one carefully planned and aimed at Dent and Batman while causing the rest of the city to enter panic mode. Meanwhile, Batman thinks he might have found a lead to The Joker thanks to Wayne Enterprises' dealings with a shady Chinese banker, and that takes Batman and Alfred to Hong Kong. The Joker has no rules, but Batman has only one, and the Joker aims to make Batman break his only rule. But who will be the one to take him out - will it be rogue vigilante Batman or will it be elected official Harvey Dent, the new hero with a face?",
+
+				 "Harry Potter is an average bespectacled 11 year old boy who has lived with the Dursley family ever since his parents died in a car crash. For some reason the family has always mistreated him. On his 11th birthday a giant man named Rubeus Hagrid hands him a letter telling him that he has been accepted as a student at the Hogwarts School of Witchcraft and Wizardry. Harry learns that his parents were wizards and were killed by an evil wizard Voldemort, a truth that was hidden from him all these years. He embarks for his new life as a student, gathering two good friends Ron Weasley and Hermione Granger along the way. They soon learn that something very valuable is hidden somewhere inside the school and Voldemort is very anxious to lay his hands on it."
+
 	];
-	
-	return{
-        getWords(textNumber){
-            return words[textNumber];
-        }
-    }
-	
+
+	return {
+		getWords(textNumber) {
+			return words[textNumber];
+		}
+	}
+
 })();

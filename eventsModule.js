0d83c63db73cb77368cf2c241e2f8bb24a0d170f
Maneesha <venigallamaneesha24@gmail.com>
2018-06-16 05:47:34 +0530

    basic eventsModule created

---
+++ eventsModule.js
@@ -0,0 +1,10 @@
+var eventsModule = (function (dataModule, UIModule, certificationModule, wordsModule) {
+var addEventListeners = function(){
+	
+}
+return{
+	init:function(duration,textNumber){
+		addEventListeners();
+	}
+};
+})(dataModule, UIModule, certificationModule, wordsModule);

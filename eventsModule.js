9897950615b746594857da0ee79d4ce80b8b98cd
Maneesha <venigallamaneesha24@gmail.com>
2018-06-18 17:30:01 +0530

    changes committed

--- eventsModule.js
+++ eventsModule.js
@@ -1,6 +1,10 @@
 var eventsModule = (function (dataModule, UIModule, certificationModule, wordsModule) {
 	var addEventListeners = function () {
 
+		
+		
+		
+		
 	}
 	return {
 		init: function (duration, textNumber) {
